01db937317593c3c546069cabe6d8b1877d9f3b5
Mateusz Błaszczyk <mateusz.blaszczyk@fitqbe.com>
2019-04-18 15:37:42 +0200

    Manualne Sortowanie

---
+++ sortowanie.py
@@ -0,0 +1,19 @@
+numbers = [2, 64, 84, 22, 17, 99, 8, 77, 58, 72]
+print(numbers)
+x = numbers[0]
+
+a = 0
+
+sorted = False
+
+
+while not sorted:
+    sorted = True
+    for num in numbers:
+        if numbers.index(num) < 9  and num > numbers[(numbers.index(num) + 1)]:
+            numbers.insert(numbers.index(num), numbers[(numbers.index(num) + 1)])
+            numbers.pop(numbers.index(num)+1)
+            sorted = False
+    a += 1
+
+print(numbers)
